762614071962fc4736ed1851f0cbab1e9c4f044a
prositedeveloper <aga.davdav@yandex.ru>
2026-03-08 18:07:37 +0300

    Update project

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 'use strict';
 
+const header = document.getElementById('header');
+const intro = document.querySelector('.intro');
+
+if (header && intro) {
+    function toggleFixedHeader() {
+        const introH = intro.offsetHeight;
+        const scrollPos = window.scrollY;
+
+
+        header.classList.toggle('fixed', scrollPos > introH);
+    }
+
+    toggleFixedHeader();
+    window.addEventListener('scroll', toggleFixedHeader);
+    window.addEventListener('resize', toggleFixedHeader);
+}
+
+
 const burger = document.getElementById('burger');
 const menu = document.getElementById('menu');
 
@@ -20,7 +38,48 @@ burger.addEventListener('click', () => {
         link.addEventListener('click', () => {
             burger.classList.remove('active');
             menu.classList.remove('active');
+            
+            document.documentElement.style.overflow = '';
             document.body.style.overflow = '';
         });
+    });
+});
+
+$(document).ready(function() {
+    $('.reviews-inner').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        accessibility: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+        ]
     });
 });
